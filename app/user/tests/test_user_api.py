98e8461316f3f84defe7ee23279bc069e22c71fe
ozgursastim <sastimozgur@gmail.com>
2020-04-14 00:32:12 +0100

    Added manage user endpoint.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -7,6 +7,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 
 def create_user(**params):
@@ -115,3 +116,70 @@ class PublicUserApiTests(TestCase):
 
         self.assertNotIn('token', apiresponse.data)
         self.assertEqual(apiresponse.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        """Test by authoticantion is required for user"""
+        apiresponse = self.client.get(ME_URL)
+
+        self.assertEqual(apiresponse.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+    """Test API requests that require authentiation"""
+
+    def setUp(self):
+        """We are doing the authentication for each test that we do, so we don't need to basically
+           set the authentication every single test. We are just doing the setup
+           and then that happens automatically before each test"""
+        self.user = create_user(
+            email='test@domain.com',
+            password='testpass',
+            name='fname',
+        )
+        """We are setting up our client, this client is reusable each test"""
+        self.client = APIClient()
+        """Reusable client is using the force authenticate method to authenticate any requests
+           that the client makes with our sample user.
+           force_authenticate is a helper fonction that basically just makes it really easy
+           to simulate or  making authenticated requests."""
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test retrieving prfile for logged in used"""
+        apiresponse = self.client.get(ME_URL)
+
+        """We are checking our reponse status code 200 Ok"""
+        self.assertEqual(apiresponse.status_code, status.HTTP_200_OK)
+        """We are checking name and email information with return data"""
+        self.assertEqual(apiresponse.data, {
+            'name': self.user.name,
+            'email': self.user.email,
+        })
+
+    def test_post_manage_not_allowed(self):
+        """Test that post is not alloewd on the manage url"""
+        """We are creating sample request, we post emty object """
+        apiresponse = self.client.post(ME_URL, {})
+
+        """This is the standart response when you try and do a HTTP method that is not allowed on the API"""
+        self.assertEqual(apiresponse.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating the user profile for authenticated user"""
+
+        """We are providing a payload, value of this payload has to have different value of the authenticate process"""
+        payload = {
+            'name': 'new name',
+            'password': 'newtest123'
+        }
+        """We are making the request with this payload"""
+        apiresponse = self.client.patch(ME_URL, payload)
+
+        """We are using the refresh from DB helper function on our user to
+           update the user with the latest values from the database"""
+        self.user.refresh_from_db()
+        """We are verifying that each of the values we provided was updated."""
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        """We are making sure that it returns the HTTP 200 ok response as expected"""
+        self.assertEqual(apiresponse.status_code, status.HTTP_200_OK)
